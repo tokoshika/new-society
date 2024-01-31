c65b942552e8a9aeac15766147a7786960359fc4
pythonthm <pythonthm@gmail.com>
2024-01-31 13:44:31 +0900

    123456

--- app/page.js
+++ app/page.js
@@ -1,10 +1,22 @@
 "use client";
-import Nav from "./components/Nav";
+// import Nav from "./components/Nav";
+import "./globals.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className="home-main">
       <h1>This is Home view</h1>
+      <h1>日本と中国の違い()</h1>
+      <ul
+        className="
+      ul"
+      >
+        <li className="li">おもてなしが招く悲劇</li>
+        <li className="li">お皿への思い</li>
+        <li className="li">一人で飲むな</li>
+        <li className="li">一人で吸うな</li>
+        <li className="li">謝ったらそこで試合終了ですよ</li>
+      </ul>
     </div>
   );
 }
